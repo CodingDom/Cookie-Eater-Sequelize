e5b7784d179c4bd7d169ab54e2a6cf6d2c01e491
Dom <dominicsmith1021@gmail.com>
2019-03-05 12:10:31 -0500

    Initial set up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger',false);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Double Quarter Pounder', false);
